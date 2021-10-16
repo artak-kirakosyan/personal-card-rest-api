8f5dad029c2282c70f8b0b7bd982686b2068fd06
Artak <artakkirakosyan96@mail.ru>
2021-10-16 14:53:45 +0400

    Make middle name optional and set default to none

--- api/models.py
+++ api/models.py
@@ -8,7 +8,9 @@ class PersonalCard(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=50, blank=False, null=False)
     last_name = models.CharField(max_length=50, blank=False, null=False)
-    middle_name = models.CharField(max_length=50, blank=True, null=True)
+    middle_name = models.CharField(
+        max_length=50, blank=True, null=True, default=None
+    )
     age = models.PositiveIntegerField(null=False, blank=False)
     gender = models.CharField(
         choices=Gender.choices,
@@ -18,11 +20,3 @@ class PersonalCard(models.Model):
 
     class Meta:
         ordering = ["last_name", "name", "middle_name"]
-
-    def __repr__(self):
-        result = "PersonalCard(name=%s, last_name=%s, middle_name=%s, age=%s, gender=%s, vaccinated=%s)"
-        result %= (
-            self.name, self.last_name, self.middle_name, self.age,
-            self.gender, self.vaccinated
-        )
-        return result
